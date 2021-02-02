54cb640d249e972b0d057291e95681a2f4f6bd68
Dean Richards <dprichards1981@googlemail.com>
2021-02-02 15:28:11 +0000

    add home page about section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './index.css';
 import coffeeBag from '../../images/coffee-bag.png';
+import { GiCoffeeBeans, GiPresent } from 'react-icons/gi';
+import { FaShippingFast } from 'react-icons/fa';
 
 const Home = () => {
 	return (
@@ -54,6 +56,49 @@ const Home = () => {
 							<p>
 								Ethiopian hand-harvested blend densely packed
 								with vibrant fruit notes.
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="aboutMini">
+				<h2>why choose us?</h2>
+				<p>
+					A large part of our role is choosing which particular
+					coffees will be feature in our range. This means working
+					closely with the best coffee growers to give you more
+					impactful experience on every level
+				</p>
+
+				<div className="aboutCardsContainer">
+					<div className="aboutCard">
+						<GiCoffeeBeans />
+						<div className="aboutInfo">
+							<h3>Best Quality</h3>
+							<p>
+								Discover an endless variety of the world's best
+								artisan coffee form each of our roasters
+							</p>
+						</div>
+					</div>
+					<div className="aboutCard">
+						<GiPresent />
+
+						<div className="aboutInfo">
+							<h3>Exclusive benefits</h3>
+							<p>
+								special offers and swag when you subscribe,
+								including 30% off your first shipment
+							</p>
+						</div>
+					</div>
+					<div className="aboutCard">
+						<FaShippingFast />
+						<div className="aboutInfo">
+							<h3>Free shipping</h3>
+							<p>
+								We cover the cost and coffee is delivered fast.
+								Peak freshness: guaranteed.
 							</p>
 						</div>
 					</div>
